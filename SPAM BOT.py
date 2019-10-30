2bfa94097f1fd35463003da98edbff37b3c3ead7
hamzajaradatlol <47142963+hamzajaradatlol@users.noreply.github.com>
2019-10-30 15:59:31 +0200

    Add files via upload
    btw this is nonstop so be aware

---
+++ SPAM BOT.py
@@ -0,0 +1,7 @@
+import pyautogui
+while True:
+    pyautogui.typewrite('warning; this is a spam bot programmed by me. only 6 lines of python code lmao')
+    pyautogui.press('enter')
+    pyautogui.typewrite('WE NEEED SOMEONE FOR THE POLE')
+    pyautogui.press('enter')
+
